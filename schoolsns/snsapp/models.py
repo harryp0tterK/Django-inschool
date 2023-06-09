795dba9b50f98527e5b5d02c34fff57fca710e21
harryp0tterK <95028526+harryp0tterK@users.noreply.github.com>
2023-06-09 04:32:46 +0000

    ChangePostModel

--- schoolsns/snsapp/models.py
+++ schoolsns/snsapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Post(models.Model)
+class Post(models.Model):
     content = models.CharField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
