08caa43949434c03254b74e06a712a911bdab1a1
harryp0tterK <95028526+harryp0tterK@users.noreply.github.com>
2023-06-09 05:25:40 +0000

    AddPostForm

--- schoolsns/snsapp/views.py
+++ schoolsns/snsapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import PostForm
+ 
+def index(request):
+    form = PostForm()
+    context = {'form':form,}
+    return render(request, 'index.html', context)
 
-# Create your views here.
+def GetPostData(request):
+    if request.method == 'POST':
+        content =request.POST['content']        
+
+        return render(request, 'index.html', params)
