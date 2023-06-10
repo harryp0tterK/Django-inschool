15526680bbe6ba23af6ffbc8590b0c1c158eafec
harryp0tterK <95028526+harryp0tterK@users.noreply.github.com>
2023-06-10 02:57:33 +0000

    change-003

--- schoolsns/snsapp/views.py
+++ schoolsns/snsapp/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from .models import Post
 from .forms import PostForm
+from django.views.generic import ListView
  
-def index(request):
-    form = PostForm()
-    context = {'form':form,}
-    return render(request, 'index.html', context)
+class PostlistView(ListView):
+    """ 一覧画面を表示するView """
+    template_name = 'index.html'   # 一覧画面のHTML
+    model = Post                   # UserDataモデル(user_dataテーブルと紐づける)
+    context_object_name = 'post_list'
 
-def GetPostData(request):
-    if request.method == 'POST':
-        content =request.POST['content']        
+    def form(self,request):
+        form = PostForm()
+        context = {'form':form,}
+        return context
 
-        return render(request, 'index.html', params)
+    def GetPostData(self,request):
+        if request.method == 'POST':
+            content =request.POST['content']        
+            return content
