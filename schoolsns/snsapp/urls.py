acc32e233531aa44ea90e1d032a6c093b31ed301
harryp0tterK <95028526+harryp0tterK@users.noreply.github.com>
2023-06-14 00:47:31 +0000

    change-0614-001

--- schoolsns/snsapp/urls.py
+++ schoolsns/snsapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import PostlistView
+from .views import PostlistView,FormPostView
 from snsapp import views
  
 urlpatterns = [
     path('', PostlistView.as_view(), name='index'),
-    path('create/', views.post, name='create')
+    path('create/', FormPostView.as_view(), name='create')
 ]
--- schoolsns/snsapp/views.py
+++ schoolsns/snsapp/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from .models import Post
 from .forms import PostForm
-from django.views.generic import ListView,CreateView,TemplateView
- 
+from django.views.generic import ListView,CreateView,TemplateView,FormView
+from django.urls import reverse_lazy
+
 class PostlistView(ListView):
     """ 一覧画面を表示するView """
     template_name = 'index.html'   # 一覧画面のHTML
-    model = Post                   # UserDataモデル(user_dataテーブルと紐づける)
-    context_object_name = 'post_list'
+    model = Post
+    def list(self,request):
+        ctx = {}
+        qs = Post.objects.all()
+        ctx["post_list"] = qs
+        return render(request,'index.html',ctx)                  # UserDataモデル(user_dataテーブルと紐づける)
 
-def post(request):
+class FormPostView(CreateView):
+    # modelに基となるクラスを指定
     template_name = 'create.html'
-    form = PostForm()
-    ctx = {}
-    ctx["form"] = form
-    return render(request,'create.html',ctx)
-
+    model = Post
+    # 別途定義したフォームのクラスを指定
+    form_class = PostForm
+    # 登録完了後の遷移ページ
+    success_url = reverse_lazy('index')
     
